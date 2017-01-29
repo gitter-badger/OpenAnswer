cd15b0da99feed88be6db54e70b34707099cfc33
Ailec <aw5fa@virginia.edu>
2017-01-29 02:46:30 -0500

    Added base template, login, validation.

--- app/views.py
+++ app/views.py
@@ -1,7 +1,34 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from .forms import LoginForm
 
 @app.route('/')
 def home():
 	user = {'nickname': 'Rachel'}  #  fake user
-	return render_template('index.html', title='home', user=user)
+	posts = [ # fake array of posts
+		{
+			'author': user,
+			'body': 'Teach me web dev please.'
+		},
+		{
+			'author': user,
+			'body': 'I\'m not sure I\'m sober'
+		},
+	]
+	return render_template(
+		'index.html', title='home', user=user, posts=posts
+	)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	form=LoginForm()
+	if form.validate_on_submit():
+		flash(
+			'Login requested for OpenID=%s, remember_me=%s' % 
+			(form.openid.data, str(form.remember_me.data))
+		)
+	return render_template(
+		'login.html',
+		title='Sign in',
+		form=form
+	)
