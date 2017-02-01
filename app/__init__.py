47b5f2d453dc9c95d37e248bba8863c60a614988
Ailec <aw5fa@virginia.edu>
2017-01-31 23:45:43 -0500

    added additional database stuff.

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, logout_user, current_user
+from oauth2 import OAuthSignIn
 
 app = Flask(__name__)
 app.config.from_object('config')
--- app/models.py
+++ app/models.py
@@ -1,10 +1,22 @@
 from app import db
-
+from app import lm
 
 class User(db.Model):
         id = db.Column(db.Integer, primary_key=True)
         nickname = db.Column(db.String(16), index=True, unique=True)
         email = db.Column(db.String(128), index=True, unique=True)
+        posts = db.relationship('Post', backref='author', lazy='dynamic')
 
         def __repr__(self):
             return '<User %r>' % self.nickname
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    body = db.Column(db.String(5000))
+    timestamp = db.Column(db.DateTime)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post %r>' % self.body
+
