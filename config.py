4c320fe7e714a295293a5de936fdbb36e2673bf2
Ailec <aw5fa@virginia.edu>
2017-01-31 21:51:50 -0500

    Added info about config

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'secret'
 
@@ -5,3 +8,6 @@ OPENID_PROVIDERS = [
     {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
     {'name': 'OpenID', 'url': 'https://www.myopenid.com'},
 ]
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
