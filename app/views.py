fb22e55a122d262779742e1819cef51ef603aaf5
Ailec <aw5fa@virginia.edu>
2017-01-31 21:46:59 -0500

    reformatted files

--- app/views.py
+++ app/views.py
@@ -2,34 +2,36 @@ from flask import render_template, flash, redirect
 from app import app
 from .forms import LoginForm
 
+
 @app.route('/')
 def home():
-	user = {'nickname': 'User'}  #  fake user
-	posts = [ # fake array of posts
-		{
-			'author': user,
-			'body': 'Teach me web dev please.'
-		},
-		{
-			'author': user,
-			'body': 'I\'m not sure I\'m sober'
-		},
-	]
-	return render_template(
-		'index.html', title='home', user=user, posts=posts
-	)
+    user = {'nickname': 'User'}  # fake user
+    posts = [  # fake array of posts
+        {
+            'author': user,
+            'body': 'Teach me web dev please.'
+        },
+        {
+            'author': user,
+            'body': 'I\'m not sure I\'m sober'
+        },
+    ]
+    return render_template(
+        'index.html', title='home', user=user, posts=posts
+    )
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-	form=LoginForm()
-	if form.validate_on_submit():
-		flash(
-			'Login requested for OpenID=%s, remember_me=%s' % 
-			(form.openid.data, str(form.remember_me.data)))
-		return redirect('/')
-	return render_template(
-		'login.html',
-		title='Sign in',
-		form=form,
-		providers=app.config['OPENID_PROVIDERS']
-	)
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(
+            'Login requested for OpenID=%s, remember_me=%s' %
+            (form.openid.data, str(form.remember_me.data)))
+        return redirect('/')
+    return render_template(
+        'login.html',
+        title='Sign in',
+        form=form,
+        providers=app.config['OPENID_PROVIDERS']
+    )
--- config.py
+++ config.py
@@ -2,6 +2,6 @@ WTF_CSRF_ENABLED = True
 SECRET_KEY = 'secret'
 
 OPENID_PROVIDERS = [
-	{'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
-	{'name': 'OpenID', 'url': 'https://www.myopenid.com'},
+    {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
+    {'name': 'OpenID', 'url': 'https://www.myopenid.com'},
 ]
